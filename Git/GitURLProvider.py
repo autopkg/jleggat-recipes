3c6fbe7b9402b9df2e5af5aa348728d32f4d3a99
Jeremy Leggat <jleggat@asu.edu>
2013-10-20 12:25:52 -0700

    altered url returned

--- Git/GitURLProvider.py
+++ Git/GitURLProvider.py
@@ -49,7 +49,7 @@ class GitURLProvider(Processor):
                     "Couldn't find download URL in %s"
                     % (index_url))
 
-                return "".join((base_url, m.group("url")))
+                return m.group("url")
 
         def main(self):
             self.env['url'] = self.get_url(BASE_URL, INDEX_PAGE, re_dmg)
